4f2604cf251e60806c62e36a5492ed50f5c05bb3
Matthew Hughes <matt.h17@yahoo.com>
2024-01-31 17:51:59 +0000

    Changed secret key

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mo(*6quo!tm^s5_zv0c_0=$1plt=kb1_e9$7y1s(+3nt#88$)5'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -85,8 +85,9 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 #    }
 #}
 
+DATABASE_URL = os.getenv('DATABASE_URL')
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    'default': dj_database_url.config(),
 }
 
 
